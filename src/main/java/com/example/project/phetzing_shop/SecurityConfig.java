614dfc8dbe23f74f091257ac2da49b308736d025
krisanachai <krisanachai001@gmail.com>
2024-02-24 00:15:29 +0700

    commit-message

--- src/main/java/com/example/project/phetzing_shop/SecurityConfig.java
+++ src/main/java/com/example/project/phetzing_shop/SecurityConfig.java
@@ -25,8 +25,6 @@ import static org.springframework.security.config.Customizer.withDefaults;
 
 import java.util.Arrays;
 
-
-
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
@@ -56,17 +54,21 @@ public class SecurityConfig {
         return authConfig.getAuthenticationManager();
     }
 
+    // Add filterChain method
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf((csrf) -> csrf.disable())
                 .cors(withDefaults())
                 .sessionManagement(
                         (sessionManagement) -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests.requestMatchers(HttpMethod.POST,
-                        "/login").permitAll().anyRequest().authenticated())
-                .addFilterBefore(authenticationFilter,
-                        UsernamePasswordAuthenticationFilter.class)
+                .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
+                        .requestMatchers("/swagger-ui.html", "/swagger-ui/**", "/api-docs/swagger-config", "/api-docs")
+                        .permitAll() // อนุญาตให้ Swagger UI เข้าถึงได้
+                        .requestMatchers(HttpMethod.POST, "/login").permitAll()
+                        .anyRequest().authenticated())
+                .addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .exceptionHandling((exceptionHandling) -> exceptionHandling.authenticationEntryPoint(exceptionHandler));
+
         return http.build();
     }
 
